fa5266798b97e0675cdfef942ff9ed1c0ed7505f
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-01-04 18:57:43 -0300

    feat: handling exceptions and throwing HTTP404

---
+++ src/main/java/com/schedulingservice/backend/web/GeneralExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.schedulingservice.backend.web;
+
+import com.schedulingservice.backend.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GeneralExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    private ResponseEntity<Object> handleBadRequest(BadRequestException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+}
