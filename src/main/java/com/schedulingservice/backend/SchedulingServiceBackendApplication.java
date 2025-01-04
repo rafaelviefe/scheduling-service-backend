36efabda7d66457c7b81ceec38cb0a6ec60148d8
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-01-03 21:22:20 -0300

    spring initializr

---
+++ src/main/java/com/schedulingservice/backend/SchedulingServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.schedulingservice.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingServiceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedulingservice/backend/SchedulingServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedulingservice.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
