84ba1f8fbf430813dc326d825fc543101235a8a1
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-01-03 22:39:55 -0300

    feat: create service layer

---
+++ src/main/java/com/schedulingservice/backend/service/SchedulingService.java
@@ -0,0 +1,43 @@
+package com.schedulingservice.backend.service;
+
+import com.schedulingservice.backend.entity.Scheduling;
+import com.schedulingservice.backend.repository.SchedulingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class SchedulingService {
+
+    private SchedulingRepository schedulingRepository;
+
+    public SchedulingService(SchedulingRepository schedulingRepository) {
+        this.schedulingRepository = schedulingRepository;
+    }
+
+    public List<Scheduling> create(Scheduling scheduling) {
+        schedulingRepository.save(scheduling);
+        return list();
+    }
+
+    public List<Scheduling> list() {
+        return schedulingRepository.findAll();
+    }
+
+    public List<Scheduling> get(UUID id) {
+        schedulingRepository.findById(id);
+        return list();
+    }
+
+    public List<Scheduling> update(Scheduling scheduling) {
+        schedulingRepository.save(scheduling);
+        return list();
+    }
+
+    public List<Scheduling> delete(UUID id) {
+        schedulingRepository.deleteById(id);
+        return list();
+    }
+
+}
