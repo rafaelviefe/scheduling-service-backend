605a64addcd9634493b5adf895cf15143ad22523
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-01-03 21:55:41 -0300

    fix: removed unnecessary imports

--- src/main/java/com/schedulingservice/backend/repository/SchedulingRepository.java
+++ src/main/java/com/schedulingservice/backend/repository/SchedulingRepository.java
@@ -3,7 +3,6 @@ package com.schedulingservice.backend.repository;
 import com.schedulingservice.backend.entity.Scheduling;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
 
 public interface SchedulingRepository extends JpaRepository<Scheduling, String> {
 
