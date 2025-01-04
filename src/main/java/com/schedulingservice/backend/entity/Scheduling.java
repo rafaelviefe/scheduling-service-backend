142615b9f32b1fb359fc76e4cefca0f4ec6d6df7
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-01-03 22:20:00 -0300

    feat: preparing to receive the database

--- src/main/java/com/schedulingservice/backend/entity/Scheduling.java
+++ src/main/java/com/schedulingservice/backend/entity/Scheduling.java
@@ -9,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Data
@@ -20,7 +21,7 @@ public class Scheduling {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    private UUID id;
 
     @NotNull
     private LocalDateTime scheduledDateTime;
--- src/main/java/com/schedulingservice/backend/repository/SchedulingRepository.java
+++ src/main/java/com/schedulingservice/backend/repository/SchedulingRepository.java
@@ -3,7 +3,9 @@ package com.schedulingservice.backend.repository;
 import com.schedulingservice.backend.entity.Scheduling;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
 
-public interface SchedulingRepository extends JpaRepository<Scheduling, String> {
+
+public interface SchedulingRepository extends JpaRepository<Scheduling, UUID> {
 
 }
