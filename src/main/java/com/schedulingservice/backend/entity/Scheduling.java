d29a56ef1c17555b0f9e9ebe05743a9a10fd6128
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2025-01-03 21:46:49 -0300

    feat: create Scheduling entity

---
+++ src/main/java/com/schedulingservice/backend/entity/Scheduling.java
@@ -0,0 +1,38 @@
+package com.schedulingservice.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "schedulings")
+public class Scheduling {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private LocalDateTime scheduledDateTime;
+
+    @NotBlank
+    private String recipient;
+
+    @NotBlank
+    private String message;
+
+    @NotNull
+    private String communicationType;
+
+}
